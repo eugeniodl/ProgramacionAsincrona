c9290707b29dc24c4e95e73a7f1668c6fc230909
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-04-17 07:23:09 -0600

    Modificar programa con instrucciones de nivel superior.

--- Ejemplo01/Program.cs
+++ Ejemplo01/Program.cs
@@ -1,34 +1,25 @@
-﻿namespace Ejemplo01
+﻿Console.WriteLine("Preparando café");
+await CalentarAguaAsync();
+await BatirCafeAsync();
+PrepararCafe();
+
+async Task CalentarAguaAsync()
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Preparando café");
-            await CalentarAguaAsync();
-            await BatirCafeAsync();
-            await PrepararCafeAsync();
-        }
+    Console.WriteLine("Calentando agua...");
+    await Task.Delay(3000); // Simulación de calentar agua durante 3 segundos
+    Console.WriteLine("Agua caliente lista");
+}
 
-        private static async Task CalentarAguaAsync()
-        {
-            Console.WriteLine("Calentando agua...");
-            await Task.Delay(3000); // Simulación de calentar agua durante 3 segundos
-            Console.WriteLine("Agua caliente lista");
-        }
+async Task BatirCafeAsync()
+{
+    Console.WriteLine("Batiendo café...");
+    await Task.Delay(2000); // Simulación de batir café durante 2 segundos
+    Console.WriteLine("Café batido listo");
+}
 
-        private static async Task BatirCafeAsync()
-        {
-            Console.WriteLine("Batiendo café...");
-            await Task.Delay(2000); // Simulación de batir café durante 2 segundos
-            Console.WriteLine("Café batido listo");
-        }
-
-        private static async Task PrepararCafeAsync()
-        {
-            Console.WriteLine("Vertiendo café en la taza...");
-            await Task.Delay(1000); // Simulación de verter café durante 1 segundo
-            Console.WriteLine("Café preparado");
-        }
-    }
+void PrepararCafe()
+{
+    Console.WriteLine("Vertiendo café en la taza...");
+    Task.Delay(1000).Wait(); // Simulación de verter café durante 1 segundo
+    Console.WriteLine("Café preparado");
 }
